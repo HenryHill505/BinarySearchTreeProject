7e2c05db1993daa5dad8625e06bbe1e8bd358e7d
HenryHill505 <hhill505@gmail.com>
2018-07-10 14:35:05 -0500

    Created class BinarySearchTreeNode<T>

---
+++ BinarySearchTree/BinarySearchTreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTreeNode<T>
+    {
+        BinarySearchTreeNode<T> LeftChild;
+        BinarySearchTreeNode<T> RightChild;
+        T value;
+
+        public BinarySearchTreeNode(T value)
+        {
+            this.value = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+    }
+}
